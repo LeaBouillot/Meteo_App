d565344ae694303f32af4ec34e091965e7a12fb8
bestdevelope <leabouillot7@gmail.com>
2024-06-14 01:08:38 +0200

    API et Icon

---
+++ script.js
@@ -0,0 +1,8 @@
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//https://openweathermap.org/img/wn/10d@2x.png
+
+const apiKey = '8f5a3e415a8ad1d13e13e5f89b4b1cab';
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const msg = form.querySelector('msg');
+const weather = document.querySelector('weather');
